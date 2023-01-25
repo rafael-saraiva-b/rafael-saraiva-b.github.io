4a053bd0cf7c94ce9695639b11195ff25dce3e6f
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2023-01-24 21:20:50 -0300

    add pagina de contato ainda nao preenchida

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { Route } from 'react-router-dom';
 import Primeira from './pages/Primeira'
 import Sobre from './pages/Sobre'
+import Contato from './pages/Contato'
 import './App.css';
 
 function App() {
   return (
     <>
       <Route path="/sobre" component={Sobre} />
+      <Route path="/contato" component={Contato}/>
       <Route exact path="/" component={Primeira}/>
     </>
   );
