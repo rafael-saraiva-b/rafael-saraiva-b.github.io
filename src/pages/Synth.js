eed9da17e14ffb3b0763c2c8db41a6543ea687bc
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2023-01-25 13:37:15 -0300

    problemas com reactronica

--- src/pages/Synth.js
+++ src/pages/Synth.js
@@ -1,13 +1,61 @@
 import React from "react";
 import Circles from '../services/Circles'
-import { useState } from "react";
+import { useState,useEffect } from "react";
+import { Song, Track, Instrument } from 'reactronica';
+
 
 function Synth()  {
+  const [totalKick,setTotalKick]= useState(10);
+  const MAX = 32;
+  const [beatKick,setBeatKick]=useState(4);
+  const [circles,setCircle]=useState(Circles(totalKick,beatKick))
+  const [notes,setNotes]=useState();
+
+  useEffect(()=>{setCircle(Circles(totalKick,beatKick))},[totalKick,beatKick])
+
+  useEffect(()=>{if(totalKick<beatKick){setBeatKick(totalKick)}},[totalKick,beatKick])
+
     return (
       <>
       <form>
-        <input type="range"/>
+        <input
+         type="range"
+         min='2'
+         max={MAX}
+         onChange={(e)=>setTotalKick(parseInt(e.target.value))}
+         value={totalKick}/>
+         <p>{totalKick}</p>
+         <input
+         type="range"
+         min='1'
+         max={totalKick}
+         onChange={(e)=>setBeatKick(parseInt(e.target.value))}
+         value={beatKick}/>
+         <p>{beatKick}</p>
       </form>
+      <p>{circles}</p>
+
+
+      <button
+      onMouseDown={()=> setNotes(['D3'])}
+      onMouseUp={()=>setNotes([])}
+      >
+        kick   
+      </button>
+
+      <Song>
+        <Track>
+         <Instrument 
+          type="sampler"
+          samples={{D3: '/st2_kick_one_shot_low_punch_basic.wav'}}
+          notes={notes}
+          onLoad={(buffers)=>{
+            console.log(buffers)
+            console.log('deu')
+          }}
+        />
+        </Track>
+      </Song>
       </>
     );
   };
