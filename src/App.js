14ccc48a8896aa1828f0de67298375f1b93aa2bb
Rafael Saraiva <rafael.saraiva.borges@gmail.com>
2023-02-28 19:23:45 -0300

    foca o portifolio no synth

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Route } from 'react-router-dom';
-import Primeira from './pages/Primeira'
 import Sobre from './pages/Sobre'
 import Contato from './pages/Contato'
 import Synth from './pages/Synth'
@@ -10,8 +9,7 @@ function App() {
     <>
       <Route path="/sobre" component={Sobre} />
       <Route path="/contato" component={Contato}/>
-      <Route path="/synth" component={Synth}/>
-      <Route exact path="/" component={Primeira}/>
+      <Route exact path="/" component={Synth}/>
     </>
   );
 }
